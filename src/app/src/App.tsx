ab4ffec44e02d04614e86b3b76c7077ede44c23c
bhardwajdevesh092005 <b23cs1014@iitj.ac.in>
2025-03-13 11:31:15 +0530

    Frontend SetUp

---
+++ src/app/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='flex justify-center items-center h-screen bg-gray-800 text-white text-4xl'>
+      Devesh Bhardwaj
+    </div>
+  )
+}
+
+export default App
